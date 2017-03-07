b79da6da1beafcc538bdbfc8b0d2a1f7c84f4bbf
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-03-07 18:15:31 +0000

    Add logic to set AWS_DEFAULT_REGION in run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@
 if [[ -z "${TOPIC}" ]]; then #If TOPIC is not set generate invalid SNS Topic name 'environment_variable_topic_unset' that flags warning in alarm settings
   REGION=$(curl -s --connect-timeout 3 http://169.254.169.254/latest/meta-data/placement/availability-zone/)
   if [[ ! -z ${REGION} ]]; then
-    REGION=${REGION:0:-1}
+    export AWS_DEFAULT_REGION=${REGION:0:-1}
   else
-    REGION="eu-west-1"
+    export AWS_DEFAULT_REGION="eu-west-1"
   fi
-  TOPIC="arn:aws:sns:${REGION}:account:environment_variable_topic_unset"
+  TOPIC="arn:aws:sns:${AWS_DEFAULT_REGION}:account:environment_variable_topic_unset"
 fi
 /collective/cloudwatch-alarms/put_metric_alarm.py --namespace ${NAMESPACE} --instanceid ${INSTANCEID} --topic ${TOPIC}
 /usr/sbin/collectd -f
