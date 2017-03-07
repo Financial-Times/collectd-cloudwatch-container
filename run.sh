7ef5250aa34d4d9bdd32279e6f2832536ff0305e
Jussi.Heinonen <jussi.heinonen@ft.com>
2017-03-07 17:42:37 +0000

    Add logic to run.sh to generate SNS Topic environment_variable_topic_unset if environment variable TOPIC is not set at launch

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
+#
+# A wrapper script that creates CloudWatch Alarms and starts collectd daemon
+
+if [[ -z "${TOPIC}" ]]; then #If TOPIC is not set generate invalid SNS Topic name 'environment_variable_topic_unset' that flags warning in alarm settings
+  REGION=$(curl -s --connect-timeout 3 http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+  if [[ ! -z ${REGION} ]]; then
+    REGION=${REGION:0:-1}
+  else
+    REGION="eu-west-1"
+  fi
+  TOPIC="arn:aws:sns:${REGION}:account:environment_variable_topic_unset"
+fi
 /collective/cloudwatch-alarms/put_metric_alarm.py --namespace ${NAMESPACE} --instanceid ${INSTANCEID} --topic ${TOPIC}
 /usr/sbin/collectd -f
